ffe29a84d710c5413e04d7c05eeae5c33af8c0a3
lee mingyu <alsrb020202@gmail.com>
2024-09-25 19:18:30 +0900

    이민규
    이민규

--- Project2/소스.cpp
+++ Project2/소스.cpp
@@ -1,1 +1,16 @@
-#include
+#include<stdio.h>
+
+int main(void)
+{
+	double ft=0.0;
+	double inch=0.0;
+	double kg=0.0;
+
+	printf("변환할 피트 인치 킬로그램 입력 :");
+	scanf_s("%lf %lf %lf", &ft, &inch, &kg);
+	printf("ft,m : %.4lf\n", ft * 1 / 3.28);
+	printf("inch,m : %.4lf\n", inch * 1 / 39.37);
+	printf("kg,slug : %.4lf\n", kg * 1 / 14.59);
+
+		return 0;
+}
