bf01e46ef19490f84a1192dc05c2e0901bf499ac
lee mingyu <alsrb020202@gmail.com>
2024-10-23 17:56:13 +0900

    10-23

--- 구구단/소스.cpp
+++ 구구단/소스.cpp
@@ -4,13 +4,33 @@ int main(void)
 {
 	int a=1;//입력될 수
 	int b;//증가할 수
-
-	printf("몇단인지 입력 :");
+	int i;
+	int j;
+	printf("몇단을 출력할 것인지 입력(전체를 보려면 10을 입력) :");
 	scanf_s("%d", &a);
 
-	for (b = 1; b <= 9; b++)//b를증감시키며 1~9까지반복
+	switch (a)
 	{
-		printf("%d*%d=%d\n", a, b, (a * b));
+	case 10:
+
+		for (i = 2; i <= 9; i++)
+		{
+			printf("%d단:\n", i);// 1단부터 9단까지 반복
+
+			for (j = 1; j <= 9; j++)
+			{
+				printf("%d x %d = %d\n", i, j, i * j);// 1부터 9까지 곱하기
+			}
+		}
+		break;
+
+	default:
+
+		for (b = 1; b <= 9; b++)//b를증감시키며 1~9까지반복
+		{
+			printf("%d*%d=%d\n", a, b, (a * b));
+			
+		}
 	}
 	return 0;
-}
+	}
